b6f089b787aa612219af81a57981cb1432384a1a
malavika <atlamalavika@gmail.com>
2023-09-20 12:48:01 +0530

    remove char from string

---
+++ myAssignments/remove_char_19_09.py
@@ -0,0 +1,11 @@
+# 1.remove given character from string
+word=input("enter_value: ")
+given_char=input("enter_char: ")
+new_word=""
+for i in word:
+    if i==given_char:
+        new_word+=""
+    else:
+        new_word=new_word+i
+        
+print(new_word)
