392fbdacdad12ac0102f19a3e20d42a253f30c53
malavika <atlamalavika@gmail.com>
2023-09-26 10:16:24 +0530

    find sum of 2 list

---
+++ assignment_25_09/2.find_sum.py
@@ -0,0 +1,10 @@
+#1 find sum of 2 list
+list1=eval(input("enter:list1: "))
+sum_of_list=0
+for i in list1:
+    sum_of_list+=i
+print(sum_of_list)
+
+#2
+list2=eval(input("enter:list2: "))
+print(sum(list2))
