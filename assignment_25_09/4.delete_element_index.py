2d7248b81ddc8a09a440ae89a318d3b63cc91c62
malavika <atlamalavika@gmail.com>
2023-09-26 10:46:42 +0530

    delete element at particular index

---
+++ assignment_25_09/4.delete_element_index.py
@@ -0,0 +1,11 @@
+#1 delete element at given index
+list1=eval(input("enter:list1: "))
+index1=int(input("num: "))
+list1.pop(index1)
+print(list1)
+
+#2
+list2=eval(input("enter:list2: "))
+index2=int(input("num: "))
+del list2[index2]
+print(list2)
