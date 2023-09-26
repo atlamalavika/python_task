d0acafcfd1cde50c10d99a193ed956229fe89e9b
malavika <atlamalavika@gmail.com>
2023-09-26 10:08:52 +0530

    merge 2 list

---
+++ assignment_25_09/1.merge_2_list.py
@@ -0,0 +1,10 @@
+#1 merge 2 list
+list1=eval(input("enter:list1: "))
+list2=eval(input("enter:list2: "))
+list1.extend(list2)
+print(list1)
+
+#2 concatination
+list3=eval(input("enter:list3: "))
+list4=eval(input("enter:list4: "))
+print(list3+list4)
