2c0d01088a4d008276bf31c962bab531975d581c
malavika <atlamalavika@gmail.com>
2023-10-14 11:21:56 +0530

    key exist

---
+++ assignment_13_10/key_exist.py
@@ -0,0 +1,17 @@
+# check key exist in dictionary 
+dict={"emply1":"malavika","id":101,"salary":20000,"age":20}
+given_key=(input("enter: "))
+print(given_key)
+count=0
+for i in dict:
+    if i==given_key:
+        count=count+1
+if count==1:
+    print("Given key exist in dictionary")
+else:
+    print("Given key does not exist in dictionary")
+        
+        
+  
+        
+
