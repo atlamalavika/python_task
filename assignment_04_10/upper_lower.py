91ad92b6e77b1079eb24893c01e06e1a52c9efae
malavika <atlamalavika@gmail.com>
2023-10-16 12:13:01 +0530

    functions

---
+++ assignment_04_10/upper_lower.py
@@ -0,0 +1,14 @@
+#count upper and lower case characters
+def count_char(x):
+    upper=0
+    lower=0
+    for i in x:
+        if i.isupper():
+            upper+=1
+        if i.islower():
+            lower+=1
+    print("No. of Upper case charaters : " +str(upper))
+    print("No. of Upper case charaters : " +str(lower))
+    
+input_string=input("enter: ")
+count_char(input_string)
