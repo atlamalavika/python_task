fc7c48f648e102489921b3eaa682cae118f79e61
malavika <atlamalavika@gmail.com>
2023-12-03 16:20:03 +0530

    encap

--- assignment_25_11/encapsulation.py
+++ assignment_25_11/encapsulation.py
@@ -1,11 +1,17 @@
 #Encapsulation : Bundling of methods and variables together 
+#variables and methods can be public or private
+#by default they are public we can accesse anywhere 
+#if private we cannot access outside the class
 class Encapsulation1:
+    b=10
     def __init__(self):
         print("this is constructor")
     def method(self):
         print("This is parent class")
 obj=Encapsulation1()
+print(obj.b)
 obj.method()
+print("______")
 
 class Encapsulation:
     __a=10  #private variable
@@ -13,4 +19,5 @@ class Encapsulation:
         print("This is parent class")
         print(Encapsulation.__a)
 obj=Encapsulation()
+# print(obj.a) :error
 obj.method()
