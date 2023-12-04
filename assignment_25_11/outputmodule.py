782118931cd9d6a2b92203b79a473ec219c651e4
malavika <atlamalavika@gmail.com>
2023-12-04 18:28:27 +0530

    jshgdj

--- assignment_25_11/outputmodule.py
+++ assignment_25_11/outputmodule.py
@@ -1,11 +1,19 @@
 
 #to know number of functions in module
-import module
-print(dir(module))
+# import module
+# print(dir(module))
 
 #math:inbuilt module which having several functions
 #used to perform mathematical operations 
-from math import *
-print(sqrt(4))
-print(ceil(10.1))
-print(floor(10.9))
+# from math import *
+# print(sqrt(4))
+# print(ceil(10.1))
+# print(floor(10.9))
+
+#random
+from random import *
+
+print(random())
+print(randint(1,100))
+a1=("mala",1,2,3)
+print(choice((2,3,4,5)))
